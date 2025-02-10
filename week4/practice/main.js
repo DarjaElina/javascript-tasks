46aa53e04d2837f91d685fc97f44fc3d1d58e621
DarjaElina <daria.elina98@gmail.com>
2025-02-10 14:21:58 +0200

    add week 4 practice folder

---
+++ week4/practice/main.js
@@ -0,0 +1,43 @@
+const textExample = document.querySelector('p');
+const textExample2 = document.querySelectorAll('.my_class');
+const textExample3 =document.querySelector('#my_id');
+
+console.log(textExample)
+console.log(textExample2)
+console.log(textExample3)
+
+// // selector
+// const btn = document.getElementById('insert_btn');
+// const input = document.getElementById('input');
+// const text = document.getElementById('text');
+
+// // function
+// const handleClick = () => {
+//     alert('Button was clicked');
+// };
+
+// const displayUsername = () => {
+//     const inputValue = input.value;
+//     text.textContent = `User entered: ${inputValue}`;
+// };
+
+// // trigger
+// btn.addEventListener('click', displayUsername);
+
+
+
+
+// Monday Practice
+// const someCoolFunction = () => {
+//     alert('Some cool text inside');
+// };
+// const anotherCoolFunction = () => {
+//     alert('I told you not to click!')
+// }
+
+// const circle = document.getElementById("alert")
+// const headings = document.getElementsByTagName('h1');
+// const paragraphs = document.getElementsByTagName('p');
+// console.log(circle.textContent);
+// console.log(headings[0].textContent);
+// console.log(paragraphs[0].textContent);
