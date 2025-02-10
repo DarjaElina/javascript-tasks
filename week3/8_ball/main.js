ec756f49a1fbc16077e8b71b5f18fa7ed3808302
DarjaElina <daria.elina98@gmail.com>
2025-02-10 14:21:31 +0200

    add 8_ball version with DOM elements and simple styling

--- week3/8_ball/main.js
+++ week3/8_ball/main.js
@@ -2,33 +2,40 @@ const getRandomInt = (min, max) => {
 return Math.floor(Math.random() * (max - min + 1) + min);
 };
 const random = getRandomInt(1, 8);
-const question = prompt('Ask me something');
-console.log(question);
+const box = document.getElementById('answer');
 const magic8Ball = (random) => {
-    switch (random) {
-        case 1:
-            return "Yes, definitely!";
-        case 2:
-            return "It is certain.";
-        case 3:
-            return "Reply hazy, try again.";
-        case 4:
-            return "Ask again later.";
-        case 5:
-            return "Better not tell you now.";
-        case 6:
-            return "My sources say no.";
-        case 7:
-            return "Outlook not so good.";
-        case 8:
-            return "Signs point to yes.";
-    };
+    const answer = document.getElementById('answer');
+    const question = document.getElementById('user-input').value;
+    if (question) {
+        switch (random) {
+            case 1:
+                answer.innerText = "Yes, definitely!";
+                break;
+            case 2:
+                answer.innerText = "It is certain";
+                break;
+            case 3:
+                answer.innerText = "Reply hazy, try again.";
+                break;
+            case 4:
+                answer.innerText = "Ask again later.";
+                break;
+            case 5:
+                answer.innerText = "Better not tell you now.";
+                break;
+            case 6:
+                answer.innerText = "My sources say no.";
+                break;
+            case 7:
+                answer.innerText = "Outlook not so good.";
+                break;
+            case 8:
+                answer.innerText = "Signs point to yes.";
+                break;
+            default: return '';
+        };
+    }
 };
 
-if (question) {
-    const res = magic8Ball(random);
-    console.log(res);
-    alert(res);
-};
-
-
+const button = document.getElementById('submit-button');
+button.addEventListener('click', () => magic8Ball(random));
