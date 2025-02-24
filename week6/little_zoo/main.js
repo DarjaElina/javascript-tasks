74b4dbb56e2277490f95f0ebcd6139aa37e841e1
DarjaElina <daria.elina98@gmail.com>
2025-02-24 22:30:59 +0200

    add week6/little_zoo version 1

--- week6/little_zoo/main.js
+++ week6/little_zoo/main.js
@@ -1,8 +1,10 @@
 const animalList = document.querySelector('#animalList');
 const searchbar = document.querySelector('#searchAnimal');
 const newAnimalName = document.querySelector('#newAnimalName');
-const newAnimalType = document.querySelector('#filterType');
 const newAnimalBtn = document.querySelector('#addAnimal');
+const errorMessage = document.querySelector('#errorMessage');
+const sortBtn = document.querySelector('#sortAnimals');
+const filterType = document.querySelector('#filterType');
 let animals = [
     {
         name: "Fox",
@@ -26,34 +28,61 @@ let animals = [
     }
 ]
 
+const capitalizeFirstLetter = (string) => {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+}
+
+const setAnimalTypeAndName = (animal, type, name) => {
+    animal.textContent = name;
+    animal.dataset.type = type;
+}
+
 animals.forEach(a => {
     const animalItem = document.createElement('li');
     animalList.appendChild(animalItem);
-    animalItem.textContent = a.name;
-})
+    setAnimalTypeAndName(animalItem, a.type, a.name);
+});
 
-const searchAnimalByName = (e) => {
-    animals.filter(a => a.name.toLowerCase().includes(e.target.value.toLowerCase()));
+const searchAnimal = (e) => {
     const animalItems = document.querySelectorAll('li');
+    const type = document.querySelector('#filterType').selectedOptions[0].value;
+    const searchQuery = e.target.value.toLowerCase();
+    console.log(type, searchQuery)
     for (const animalItem of animalItems) {
-        if (!animalItem.textContent.toLowerCase().includes(e.target.value.toLowerCase()))
-        {
-            animalList.removeChild(animalItem);
-            animalItem.textContent = '';
-        }
+        if (!animalItem.textContent.toLowerCase().includes(searchQuery) && (animalItem.dataset.type !== type && type !== 'All'))
+            animalItem.style.display = 'none';
+        else 
+            animalItem.style.display = 'flex';
     }
 }
 
-
-searchbar.addEventListener('input', searchAnimalByName)
-
-newAnimalBtn.addEventListener('click', () => {
-    if (!newAnimalName.value) {
-        console.log('nope')
+const addAnimal = () => {
+    const name = newAnimalName.value;
+    if (!name || name.length < 3) {
+        errorMessage.textContent = "Please enter valid animal's name";
         return;
     }
-    animals.push({ name: newAnimalName.value })
+    const type = document.querySelector('#newAnimalType').selectedOptions[0].value;
+    const capitalizedAnimalName = capitalizeFirstLetter(name);
+    animals.push({ name, type })
     const animalItem = document.createElement('li');
     animalList.appendChild(animalItem);
-    animalItem.textContent = newAnimalName.value;
-})
+    setAnimalTypeAndName(animalItem, type, capitalizedAnimalName);
+    newAnimalName.value = '';
+    errorMessage.textContent = '';
+}
+
+const sortAnimals = () => {
+    let sortedAnimals = animals.sort((a, b) => a.name.localeCompare(b.name))
+    const animalItems = document.querySelectorAll('li');
+    animalItems.forEach((animalItem, index) => animalItem.textContent = sortedAnimals[index].name);
+}
+
+
+searchbar.addEventListener('input', searchAnimal)
+
+newAnimalBtn.addEventListener('click', addAnimal);
+
+sortBtn.addEventListener('click', sortAnimals);
+
+filterType.addEventListener('change', searchAnimal);
